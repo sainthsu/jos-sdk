0a88d929e5704aeac4d4c389e0b81a640765e11f
saint <saint@aliyun.com>
2014-01-08 18:05:10 +0800

    add request

--- request/request.js
+++ request/request.js
@@ -2,7 +2,25 @@
  * New node file
  */
 var request = function() {
-	
+	this.apiMethod = null;
+	this.apiParams = {};
+};
+
+request.prototype.set = function(setting,val){
+	if (1 === arguments.length)
+	{
+		return this.apiParams[setting];
+	}
+	else
+	{
+		this.apiParams[setting] = val;
+		return this;
+	}
+};
+
+request.prototype.use = function(method) {
+	this.api = require("./"+method);
+	this.apiMethod = this.api.getMethod();
 };
 
 module.exports = new request();
