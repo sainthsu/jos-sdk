6f327ea01ced9d694577858f51c7d3e9769785d1
saint <saint>
2014-04-21 17:09:50 +0800

    add baserequest

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
  * Created by saint on 1/11/14.
  */
 
-
 var jos = require('./lib/jos');
 jos.API = require('./lib/request');
 jos.OAuth = require('./lib/oauth');
---
+++ request/BaseRequest.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Administrator on 14-4-21.
+ */
+var BaseRequest={
+    getMethod:function (){
+        return this.method;
+    },
+    checkParams:function (apiParams) {
+        var params = apiParams;
+        this.check.forEach(function(element){
+            if(element.equal('require'))
+            {
+                if(params[element] === undefined)
+                {
+                    return new Error("apiParams:"+element+"which needed is undefined;");
+                }
+            }
+        });
+    }
+};
+
+module.exports=new BaseRequest();
