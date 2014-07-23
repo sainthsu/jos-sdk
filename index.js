915721d12eb92728ab5296ddddbec88cbfdd2351
saint hsu <saint@aliyun.com>
2014-07-23 17:11:46 +0800

    modify index

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
  * Created by saint on 1/11/14.
  */
 
-var jos = require('./lib/jos');
-jos.API = require('./api/index');
-jos.OAuth = require('./lib/oauth');
-module.exports = jos;
+var JOS = require('./lib/jos');
+JOS.API = require('./api/index');
+module.exports = JOS;
